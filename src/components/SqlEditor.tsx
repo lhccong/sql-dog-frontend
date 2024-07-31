da885d2bacc60677513b3533c3c8cc8c48ace73b
lhccong <771901874@qq.com>
2024-07-31 20:49:13 +0800

    feat：引入sql-formatter插件以及封装SqlEditor组件

--- src/components/SqlEditor.tsx
+++ src/components/SqlEditor.tsx
@@ -1,88 +1,47 @@
-// import React, {useRef, useEffect, useState, useCallback} from "react";
-// import MonacoEditor from "react-monaco-editor";
-// import {Button, Space, message} from "antd";
-// import {format} from "sql-formatter";
-// import {initDB, runSQL} from "../core/sqlExecutor";
-// // import {QueryExecResult} from "sql.js";
-//
-// const SqlEditor = ({level, editorStyle, resultStatus, onSubmit}) => {
-//   const editorRef = useRef(null);
-//   const [db, setDb] = useState(null);
-//
-//   useEffect(() => {
-//     // 初始化 DB
-//     const initDatabase = async () => {
-//       const database = await initDB(level.initSQL);
-//       setDb(database);
-//       doSubmit();
-//     };
-//     initDatabase();
-//   }, [level]);
-//
-//   useEffect(() => {
-//     // 初始化默认 SQL
-//     if (editorRef.current) {
-//       editorRef.current.editor.setValue("-- 请在此处输入 SQL\n" + level.defaultSQL);
-//     }
-//   }, [level]);
-//
-//   const doFormat = useCallback(() => {
-//     if (!editorRef.current) {
-//       return;
-//     }
-//     const inputStr = editorRef.current.editor.getValue();
-//     const resultStr = format(inputStr, {language: "sqlite"});
-//     editorRef.current.editor.setValue(resultStr);
-//   }, []);
-//
-//   const doReset = useCallback(() => {
-//     if (editorRef.current) {
-//       editorRef.current.editor.setValue(level.defaultSQL);
-//       doSubmit();
-//     }
-//   }, [level]);
-//
-//   const doSubmit = useCallback(() => {
-//     if (!editorRef.current) {
-//       return;
-//     }
-//     const inputStr = editorRef.current.editor.getValue();
-//     try {
-//       const result = runSQL(db, inputStr);
-//       const answerResult = runSQL(db, level.answer);
-//       onSubmit?.(inputStr, result, answerResult);
-//     } catch (error) {
-//       message.error("语句错误，" + error.message);
-//       onSubmit?.(inputStr, [], [], error.message);
-//     }
-//   }, [db, level, onSubmit]);
-//
-//   return (
-//     <div id="sqlEditor">
-//       <div style={editorStyle}>
-//         <MonacoEditor
-//           ref={editorRef}
-//           width="100%"
-//           height="500"
-//           language="sql"
-//           theme="vs-dark"
-//           options={{
-//             formatOnPaste: true,
-//             automaticLayout: true,
-//             fontSize: 16,
-//             minimap: {
-//               enabled: false,
-//             },
-//           }}
-//         />
-//       </div>
-//       <Space size={16} style={{marginTop: 16}}>
-//         <Button type="primary" style={{width: 180}} onClick={doSubmit}>
-//           运行
-//         </Button>
-//         <Button onClick={doFormat}>格式化</Button>
-//         <Button onClick={doReset}>重置</Button>
-//       </Space>
-//     </div>
-//   );
-// };
+import React, {useEffect, useRef, useState} from 'react';
+import {Button} from 'antd';
+import * as monaco from "monaco-editor";
+import {format} from "sql-formatter";
+
+export const SqlEditor = ({sql = 'select * from student'}) => {
+  const [querySQL, setQuerySQL] = useState(sql);
+  const editorRef = useRef(null);
+  // @ts-ignore
+  useEffect(() => {
+    // @ts-ignore
+    editorRef.current = monaco.editor.create(document.getElementById('container'), {
+      width: "600",
+      height: "600",
+      theme: "vs-dark",
+      value: "-- 请在此处输入 SQL\n" + querySQL,
+      language: 'sql'
+    });
+  }, []);
+  const run = () => {
+    console.log('运行', querySQL);
+    // 在这里可以使用 query 进行进一步处理
+  };
+
+  const formatSQL = () => {
+    const resultStr = format(querySQL, {language: "sqlite"});
+    // @ts-ignore
+    editorRef.current.setValue(resultStr);
+    console.log('格式化' + resultStr);
+  };
+
+  const reset = () => {
+    setQuerySQL('');
+    console.log('重置');
+  };
+
+  return (
+    <div>
+      <div style={{height: 400, width: 600}} id={"container"}/>
+      <div style={{marginTop: 20}}>
+        <Button type={"primary"} style={{width: 100}} onClick={run}>运行</Button>
+        <Button style={{marginLeft: 20}} onClick={formatSQL}>格式化</Button>
+        <Button style={{marginLeft: 20}} onClick={reset}>重置</Button>
+      </div>
+    </div>
+  );
+};
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,32 +1,17 @@
-import {Button, Card, Col, Row} from 'antd';
+import {Card, Col, Row} from 'antd';
 import 'monaco-editor/min/vs/editor/editor.main.css';
-import * as monaco from 'monaco-editor';
-import React, {useEffect} from "react";
+import React from "react";
+import {SqlEditor} from "@/components/SqlEditor";
 
 const Welcome: React.FC = () => {
-  useEffect(() => {
-    // @ts-ignore
-    monaco.editor.create(document.getElementById('container'), {
-      width: "600",
-      height: "600",
-      theme: "vs-dark",
-      value: "-- 请在此处输入 SQL\n" +
-        "select * from student",
-      language: 'sql'
-    });
-  }, []);
   return (
     <>
       <Row>
-        <Col span={11}><Card>
-          <div style={{height: 400, width: 600}} id={"container"}/>
-          <div style={{marginTop: 20}}>
-            <Button type={"primary"} style={{width: 100}}>运行</Button>
-            <Button>格式化</Button>
-            <Button>重置</Button>
-          </div>
-
-        </Card></Col>
+        <Col span={11}>
+          <Card>
+            <SqlEditor/>
+          </Card>
+        </Col>
         <Col span={12} style={{marginLeft: 10}}><Card>1111</Card></Col>
       </Row>
     </>
