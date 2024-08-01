69ee6a7bd76df1268c07a60e0c6385019011e82f
lhccong <771901874@qq.com>
2024-08-01 22:00:37 +0800

    feat：引入 sql.js 来执行 SQLite 初始化

--- src/components/SqlEditor.tsx
+++ src/components/SqlEditor.tsx
@@ -2,12 +2,15 @@ import React, {useEffect, useRef, useState} from 'react';
 import {Button} from 'antd';
 import * as monaco from "monaco-editor";
 import {format} from "sql-formatter";
+import {initDB, runSQL} from "@/core/sqlExecutor";
+import {Database} from "sql.js";
 
 export const SqlEditor = ({sql = 'select * from student'}) => {
   const [querySQL, setQuerySQL] = useState(sql);
   const editorRef = useRef(null);
+  const db = useRef<Database>();
   // @ts-ignore
-  useEffect(() => {
+  useEffect(async () => {
     // @ts-ignore
     editorRef.current = monaco.editor.create(document.getElementById('container'), {
       width: "600",
@@ -16,10 +19,24 @@ export const SqlEditor = ({sql = 'select * from student'}) => {
       value: "-- 请在此处输入 SQL\n" + querySQL,
       language: 'sql'
     });
+    // 初始化 / 更新 DB
+    // @ts-ignore
+    console.log("开始初始化数据库");
+    db.current = await initDB("CREATE TABLE if not exists rewards (\n" +
+      "    adventurer_id INT,\n" +
+      "    adventurer_name VARCHAR(50),\n" +
+      "    task_id INT,\n" +
+      "    task_name VARCHAR(100),\n" +
+      "    reward_coins INT\n" +
+      ");");
+    console.log("开始执行sql")
+    const result = runSQL(db.current, "select * from rewards");
+    console.log("执行结果" + result);
   }, []);
   const run = () => {
     // @ts-ignore
     console.log('运行', editorRef.current.getValue());
+
     // 在这里可以使用 query 进行进一步处理
   };
 
---
+++ src/core/sqlExecutor.tsx
@@ -0,0 +1,43 @@
+import initSqlJs, { Database, SqlJsStatic } from "sql.js";
+
+/**
+ * SQL 执行器
+ *
+ */
+let SQL: SqlJsStatic;
+
+// 可以直接远程加载 db 文件
+// const buf = await fetch("/sql1.db").then((res) => res.arrayBuffer());
+// const db = new SQL.Database(new Uint8Array(buf));
+
+/**
+ * 获取初始化 DB
+ * @param initSql
+ */
+export const initDB = async (initSql?: string) => {
+  if (!SQL) {
+    console.log("开始加载 wasm")
+    SQL = await initSqlJs({
+      // Required to load the wasm binary asynchronously
+      locateFile: () => "./sql-wasm.wasm",
+    });
+  }
+  console.log("完成加载",SQL)
+  console.log("初始化数据库的sql:",initSql);
+  // Create a database
+  const db = new SQL.Database();
+  if (initSql) {
+    // Execute a single SQL string that contains multiple statements
+    db.run(initSql); // Run the query without returning anything
+  }
+  return db;
+};
+
+/**
+ * 执行 SQL
+ * @param db
+ * @param sql
+ */
+export const runSQL = (db: Database, sql: string) => {
+  return db.exec(sql);
+};
