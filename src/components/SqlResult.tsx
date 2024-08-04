98c9fc126681e7a17e8d41647e29fcf08898042e
lhccong <771901874@qq.com>
2024-08-04 16:17:14 +0800

    feat:新增执行结果样式

--- src/components/SqlResult.tsx
+++ src/components/SqlResult.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Card} from 'antd';
+import {Card, Tabs} from 'antd';
 import {SqlResultTable} from './SqlResultTable';
+import {AlignLeftOutlined, AreaChartOutlined, SunOutlined} from "@ant-design/icons";
 
 interface QueryExecResult {
   columns: string[];
@@ -23,25 +24,38 @@ const RESULT_STATUS_INFO_MAP: Record<number, string> = {
 };
 
 export const SqlResultCard: React.FC<Props> = ({
-                                          result = [],
-                                          // answerResult = [],
-                                          resultStatus,
-                                          errorMsg = '',
-                                          // level,
-                                        }) => {
+                                                 result = [],
+                                                 // answerResult = [],
+                                                 resultStatus,
+                                                 errorMsg = '',
+                                                 // level,
+                                               }) => {
   return (
-    <Card
-      id="sqlResult"
-      title="执行结果"
-      extra={RESULT_STATUS_INFO_MAP[resultStatus]}
-      bordered={false}
-      style={{maxHeight: '420px', overflowY: 'auto'}}
-    >
-      {!errorMsg ? (
-        <SqlResultTable result={result}/>
-      ) : (
-        <div>❌ 语句错误：{errorMsg}</div>
-      )}
-    </Card>
+    <>
+      <Tabs>
+        <Tabs.TabPane tab="执行结果" key="1" icon={<AlignLeftOutlined/>}>
+          <Card
+            id="sqlResult"
+            title="Result"
+            extra={RESULT_STATUS_INFO_MAP[resultStatus]}
+            bordered={false}
+            style={{maxHeight: '420px', overflowY: 'auto'}}
+          >
+            {!errorMsg ? (
+              <SqlResultTable result={result}/>
+            ) : (
+              <div>❌ 语句错误：{errorMsg}</div>
+            )}
+          </Card>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="执行计划" key="2" icon={<AreaChartOutlined/>}>
+          Content of Tab Pane 2
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="优化建议" key="3" icon={<SunOutlined/>}>
+          Content of Tab Pane 3
+        </Tabs.TabPane>
+      </Tabs>
+    </>
+
   );
 };
