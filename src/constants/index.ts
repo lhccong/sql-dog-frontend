fadc15513aa02d1eab5e76efe5de334844d15e7b
lhccong <liuhuaicong@zhut.cn>
2024-08-09 11:41:30 +0800

    feat：新增默认 constant 数据

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,3 +12,141 @@ export const BACKEND_HOST_LOCAL_REDIRECT = BACKEND_HOST_LOCAL + '/api/dog/';
 
 export const SYSTEM_LOGO =
   'https://img1.baidu.com/it/u=2452283928,895358163&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1706288400&t=501138dfe84a1f06f0b9e99a83032726';
+
+export const DEFAULT_NAME = 'SQL之父';
+
+/**
+ * 字段类型列表
+ */
+export const FIELD_TYPE_LIST = [
+  'tinyint',
+  'smallint',
+  'mediumint',
+  'int',
+  'bigint',
+  'float',
+  'double',
+  'decimal',
+  'date',
+  'time',
+  'year',
+  'datetime',
+  'timestamp',
+  'char',
+  'varchar',
+  'tinytext',
+  'text',
+  'mediumtext',
+  'longtext',
+  'tinyblob',
+  'blob',
+  'mediumblob',
+  'longblob',
+  'binary',
+  'varbinary',
+];
+
+/**
+ * onUpdate 值列表
+ */
+export const ON_UPDATE_LIST = ['CURRENT_TIMESTAMP'];
+
+/**
+ * 默认添加的字段信息
+ */
+export const DEFAULT_ADD_FIELD: any = {
+  fieldName: 'username',
+  comment: '用户名',
+  defaultValue: undefined,
+  fieldType: 'varchar(256)',
+  mockType: '随机',
+  mockParams: '人名',
+  notNull: true,
+  primaryKey: false,
+  autoIncrement: false,
+};
+
+/**
+ * 通用字段列表
+ */
+export const COMMON_FIELD_LIST: any[] = [
+  {
+    fieldName: 'id',
+    comment: '主键',
+    defaultValue: undefined,
+    fieldType: 'bigint',
+    mockType: '不模拟',
+    notNull: true,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  {
+    fieldName: 'create_time',
+    comment: '创建时间',
+    defaultValue: 'CURRENT_TIMESTAMP',
+    fieldType: 'datetime',
+    mockType: '不模拟',
+    notNull: true,
+    primaryKey: false,
+    autoIncrement: false,
+  },
+  {
+    fieldName: 'update_time',
+    comment: '更新时间',
+    defaultValue: 'CURRENT_TIMESTAMP',
+    fieldType: 'datetime',
+    mockType: '不模拟',
+    notNull: true,
+    primaryKey: false,
+    autoIncrement: false,
+    onUpdate: 'CURRENT_TIMESTAMP',
+  },
+  {
+    fieldName: 'is_deleted',
+    comment: '是否删除(0-未删, 1-已删)',
+    defaultValue: '0',
+    fieldType: 'tinyint',
+    mockType: '不模拟',
+    notNull: true,
+    primaryKey: false,
+    autoIncrement: false,
+  },
+];
+
+/**
+ * 模拟类型列表
+ */
+export const MOCK_TYPE_LIST = ['固定', '随机', '递增', '规则', '词库', '不模拟'];
+
+/**
+ * 模拟参数随机生成类型列表
+ */
+export const MOCK_PARAMS_RANDOM_TYPE_LIST = [
+  '字符串',
+  '整数',
+  '小数',
+  '日期',
+  '时间戳',
+  '网址',
+  'IP',
+  '邮箱',
+  '手机号',
+  '人名',
+  '城市',
+  '大学',
+];
+
+/**
+ * 审核状态枚举
+ */
+export const REVIEW_STATUS_ENUM = {
+  0: {
+    text: '待审核',
+  },
+  1: {
+    text: '通过',
+  },
+  2: {
+    text: '拒绝',
+  },
+};
