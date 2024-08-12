a7bb1cf52d21e3bad11d3d62b71eee85e05cdf61
lhccong <liuhuaicong@zhut.cn>
2024-08-12 09:27:08 +0800

    fix:移除多余前端变量

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -13,7 +13,6 @@ export const BACKEND_HOST_LOCAL_REDIRECT = BACKEND_HOST_LOCAL + '/api/dog/';
 export const SYSTEM_LOGO =
   'https://img1.baidu.com/it/u=2452283928,895358163&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1706288400&t=501138dfe84a1f06f0b9e99a83032726';
 
-export const DEFAULT_NAME = 'SQL之父';
 
 /**
  * 字段类型列表
