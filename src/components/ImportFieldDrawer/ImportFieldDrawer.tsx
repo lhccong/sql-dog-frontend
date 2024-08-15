fc169f9d4b6414213bac47e1c54161ef4efefa7b
lhccong <liuhuaicong@zhut.cn>
2024-08-15 11:47:00 +0800

    fix：导入字段前后端联调

--- src/components/ImportFieldDrawer/ImportFieldDrawer.tsx
+++ src/components/ImportFieldDrawer/ImportFieldDrawer.tsx
@@ -1,8 +1,8 @@
-// import { listMyFieldInfoByPage } from '@/services/fieldInfoService';
-import { useModel } from '@@/exports';
-import { Drawer, message } from 'antd';
+import {useModel} from '@@/exports';
+import {Drawer, message} from 'antd';
 import React from 'react';
 import FieldInfoCard from "@/components/FieldInfoCard/FieldInfoCard";
+import {listMyFieldInfoVoByPage} from "@/services/backend/fieldInfoController";
 
 interface Props {
   onImport?: (values: FieldInfoType.FieldInfo) => void;
@@ -17,8 +17,8 @@ interface Props {
  * @author https://github.com/lhccong
  */
 const ImportFieldDrawer: React.FC<Props> = (props) => {
-  const { visible, onImport, onClose } = props;
-  const { initialState } = useModel('@@initialState');
+  const {visible, onImport, onClose} = props;
+  const {initialState} = useModel('@@initialState');
   const loginUser = initialState?.currentUser;
 
   /**
@@ -33,25 +33,25 @@ const ImportFieldDrawer: React.FC<Props> = (props) => {
       setDataList: (dataList: FieldInfoType.FieldInfo[]) => void,
       setTotal: (total: number) => void,
     ) => {
-      // listMyFieldInfoByPage(searchParams)
-      //   .then((res) => {
-      //     setDataList(res.data.records);
-      //     setTotal(res.data.total);
-      //   })
-      //   .catch((e) => {
-      //     message.error('加载失败，' + e.message);
-      //   });
+      listMyFieldInfoVoByPage(searchParams)
+        .then((res) => {
+          setDataList(res.data?.records as any);
+          setTotal(res.data?.total as any);
+        })
+        .catch((e) => {
+          message.error('加载失败，' + e.message);
+        });
     }
     : undefined;
 
   return (
     <Drawer
       title="导入字段"
-      contentWrapperStyle={{ width: '60%', minWidth: 320 }}
+      contentWrapperStyle={{width: '60%', minWidth: 320}}
       open={visible}
       onClose={onClose}
     >
-      <FieldInfoCard onLoad={loadMyData} onImport={onImport} />
+      <FieldInfoCard onLoad={loadMyData} onImport={onImport}/>
     </Drawer>
   );
 };
